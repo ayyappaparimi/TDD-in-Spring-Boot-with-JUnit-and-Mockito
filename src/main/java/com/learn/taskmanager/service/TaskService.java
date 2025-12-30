431a2fce4cda3bef321ed60228eb820bac73044f
Parimi <parimiayyappa@gmail.com>
2025-12-29 19:33:33 -0600

    Implementing_the_method_in_the_service_layer

--- src/main/java/com/learn/taskmanager/service/TaskService.java
+++ src/main/java/com/learn/taskmanager/service/TaskService.java
@@ -1,10 +1,26 @@
 package com.learn.taskmanager.service;
 
 import com.learn.taskmanager.model.Task;
+import com.learn.taskmanager.repository.TaskRepository;
+import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+@Service
 public class TaskService {
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
 
     public Task updateTaskStatus(Long id, String status) {
+        Optional<Task> taskOptional = taskRepository.findById(id);
+        if(taskOptional.isPresent()) {
+            Task t = taskOptional.get();
+            t.setStatus(status);
+            return taskRepository.save(t);
+        }
         return null;
     }
 }
