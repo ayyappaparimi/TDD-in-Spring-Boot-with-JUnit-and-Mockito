9fb324c6ecb3bfba40dfc18a5a85c164fc66de75
Parimi <parimiayyappa@gmail.com>
2025-12-29 23:40:55 -0600

    Adding_the_rest_of_the_service_layer

--- src/main/java/com/learn/taskmanager/service/TaskService.java
+++ src/main/java/com/learn/taskmanager/service/TaskService.java
@@ -1,9 +1,11 @@
 package com.learn.taskmanager.service;
 
+import com.learn.taskmanager.exception.TaskNotFoundException;
 import com.learn.taskmanager.model.Task;
 import com.learn.taskmanager.repository.TaskRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -14,16 +16,34 @@ public class TaskService {
         this.taskRepository = taskRepository;
     }
 
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public Task createTask(Task task) {
+        return taskRepository.save(task);
+    }
+
     public Task updateTaskStatus(Long id, String status) {
         Task taskToUpdate = getTaskById(id);
-        if (taskToUpdate == null) {
-            return null;
-        }
         taskToUpdate.setStatus(status);
         return taskRepository.save(taskToUpdate);
     }
 
     public Task getTaskById(Long id) {
-        return taskRepository.findById(id).orElse(null);
+        return taskRepository.findById(id)
+                .orElseThrow(() -> new TaskNotFoundException("Task not found, id: " + id));
+    }
+
+    public Task updateTask(Long id, Task taskData) {
+        Task existingTask = getTaskById(id);
+        existingTask.setTitle(taskData.getTitle());
+        existingTask.setStatus(taskData.getStatus());
+        return taskRepository.save(existingTask);
+    }
+
+    public void deleteTask(Long id) {
+        Task taskToDelete = getTaskById(id);
+        taskRepository.delete(taskToDelete);
     }
 }
--- src/test/java/com/learn/taskmanager/service/TaskServiceTest.java
+++ src/test/java/com/learn/taskmanager/service/TaskServiceTest.java
@@ -1,5 +1,6 @@
 package com.learn.taskmanager.service;
 
+import com.learn.taskmanager.exception.TaskNotFoundException;
 import com.learn.taskmanager.model.Task;
 import com.learn.taskmanager.repository.TaskRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,6 +11,8 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -24,10 +27,66 @@ public class TaskServiceTest {
     @InjectMocks
     private TaskService taskService;
 
-//    @BeforeEach
-//    void setUp() {
-//        MockitoAnnotations.openMocks(this);
-//    }
+    @Test
+    void testGetAllTask() {
+        // arrange
+        List<Task> task = Arrays.asList(
+                new Task("Task 1", "To do"),
+                new Task("Task 2", "In progress")
+        );
+
+        when(taskRepository.findAll()).thenReturn(task);
+
+        // act
+        List<Task> retrievedTasks = taskService.getAllTasks();
+
+        // assert
+        assertNotNull(retrievedTasks);
+        assertEquals(2, retrievedTasks.size());
+        verify(taskRepository, times(1)).findAll();
+    }
+
+    @Test
+    void testGetTaskById() {
+        // arrange
+        Task task = new Task(1L, "Task 1", "To do");
+        when(taskRepository.findById(1L)).thenReturn(Optional.of(task));
+
+        // act
+        Task retrievedTask = taskService.getTaskById(1L);
+
+        // assert
+        assertNotNull(task);
+        assertEquals(1l, retrievedTask.getId());
+        assertEquals("Task 1", retrievedTask.getTitle());
+        assertEquals("To do", retrievedTask.getStatus());
+        verify(taskRepository, times(1)).findById(1L);
+    }
+
+    @Test
+    void testGetTaskById_TaskNotFound() {
+        // arrange
+        when(taskRepository.findById(1L)).thenReturn(Optional.empty());
+
+        // act & assert
+        assertThrows(TaskNotFoundException.class, () -> taskService.getTaskById(1L));
+        verify(taskRepository, times(1)).findById(1L);
+    }
+
+    @Test
+    void testCreateTask() {
+        // arrange
+        Task task = new Task("Test task", "To do");
+        when(taskRepository.save(any(Task.class))).thenReturn(task);
+
+        // act
+        Task createdTask = taskService.createTask(task);
+
+        // assert
+        assertNotNull(createdTask);
+        assertNotNull("Test task", createdTask.getTitle());
+        verify(taskRepository, times(1)).save(task);
+    }
 
     @Test
     void testUpdateTaskStatus() {
@@ -45,5 +104,50 @@ public class TaskServiceTest {
         assertEquals("In progress", updatedTask.getStatus());
         verify(taskRepository, times(1)).findById(1L);
         verify(taskRepository, times(1)).save(task);
+    }
+
+    @Test
+    void testUpdateTask() {
+        // arrange
+        Task existingTask = new Task(1L, "Old title", "To do");
+        Task updatedTaskData = new Task("New title", "Done");
+
+        when(taskRepository.findById(1L)).thenReturn(Optional.of(existingTask));
+        when(taskRepository.save(any(Task.class))).thenAnswer(invocation -> invocation.getArgument(0));
+
+        // act
+        Task updatedTask = taskService.updateTask(1L, updatedTaskData);
+
+
+        // assert
+        assertNotNull(updatedTask);
+        assertEquals("New title", updatedTask.getTitle());
+        assertEquals("Done", updatedTask.getStatus());
+        verify(taskRepository).save(existingTask);
+    }
+
+    @Test
+    void testDeleteTask() {
+        // arrange
+        Task existingTask = new Task(1L, "Test task to delete", "To do");
+        when(taskRepository.findById(1L)).thenReturn(Optional.of(existingTask));
+
+        // act
+        taskService.deleteTask(1L);
+
+        // assert
+        verify(taskRepository).delete(existingTask);
+        verify(taskRepository).findById(1L);
+    }
+
+    @Test
+    void testDeleteTask_TAskNotFound() {
+        // arrange
+        when(taskRepository.findById(1L)).thenReturn(Optional.empty());
+
+        // act & assert
+        assertThrows(TaskNotFoundException.class, () -> taskService.deleteTask(1L));
+        verify(taskRepository).findById(1L);
+
     }
 }
